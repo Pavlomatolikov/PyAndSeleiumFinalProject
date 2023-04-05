c9c654301630623749f64e368cd8312486e5e7a0
Pavlomatolikov <Pavlomatolikov@gmail.com>
2023-04-05 16:28:42 +0200

    Product page parametrized test was added

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main p.price_color")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > :nth-child(1) > .alertinner strong")
+    BASKET_PRICE = (By.CSS_SELECTOR, "#messages > :nth-child(3) > .alertinner strong")
---
+++ pages/product_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        add_to_basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        add_to_basket_button.click()
+
+    def product_name(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+
+    def product_price(self):
+        return self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+
+    def should_be_success_message(self):
+        added_product_name_text = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE).text
+        assert self.is_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is not presented"
+        assert self.product_name() == added_product_name_text
+
+    def should_be_correct_basket_price(self):
+        basket_price_text = self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text
+        assert basket_price_text == self.product_price()
+
