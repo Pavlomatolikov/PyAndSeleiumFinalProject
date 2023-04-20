55a641e4edd30b0cd8925543cd231cce375e1e79
Pavlomatolikov <Pavlomatolikov@gmail.com>
2023-04-20 14:08:48 +0200

    Some methods were moved to BasePage

--- pages/product_page.py
+++ pages/product_page.py
@@ -18,6 +18,14 @@ class ProductPage(BasePage):
         assert self.is_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is not presented"
         assert self.product_name() == added_product_name_text
 
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_disappear_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message did not disappear"
+
     def should_be_correct_basket_price(self):
         basket_price_text = self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text
         assert basket_price_text == self.product_price()
